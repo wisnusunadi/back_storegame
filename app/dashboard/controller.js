605fa4b8a3b850c892e5b1c6da23a4cd91a9bc89
wisnusunadi <barto7773@gmail.com>
2023-01-25 11:46:22 +0700

    final dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,21 @@
+const Transaction = require('../transaction/model')
+const Voucher = require('../voucher/model')
+const Player = require('../player/model')
+const Category = require('../category/model')
+
 module.exports = {
     index: async (req, res) => {
+        const transaction = await Transaction.countDocuments()
+        const category = await Category.countDocuments()
+        const player = await Player.countDocuments()
+        const voucher = await Voucher.countDocuments()
         try {
-            res.render('index', {
+            res.render('admin/dashboard/view_dashboard', {
                 name: req.session.user.name,
-                title: 'Dashboard'
+                title: 'Dashboard',
+                count: {
+                    transaction, category, player, voucher
+                },
             })
         } catch (error) {
             console.log(error)
